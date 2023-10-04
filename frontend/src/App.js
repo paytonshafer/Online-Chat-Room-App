eb51527f78266bd133ba70f1e838b8c1a27aec60
paytonshafer <paytonshafer@gmail.com>
2023-10-04 09:34:36 -0400

    add router and socket connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
+import socketIO from 'socket.io-client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home'
 import './App.css';
 
+const socket = socketIO.connect('http://localhost:4000');
+
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home socket={socket}/>}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
