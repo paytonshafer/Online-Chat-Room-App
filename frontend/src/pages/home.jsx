340e3bae8d0ecf99699a6477305c5beca1d72c45
paytonshafer <paytonshafer@gmail.com>
2023-10-10 13:09:41 -0400

    bold sender

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -198,7 +198,7 @@ const Home = ({ socket }) => {
                     <MDBRow end key={index} style={{marginLeft: '20rem'}}>
                         <MDBCol md='auto' className='m-0'>
                             <MDBCard background='primary' shadow='1' className='my-1' style={{color: 'white'}}>
-                                <MDBCardHeader className='p-0 p-2 text-end' >{data.split(': ')[0].slice(10)}</MDBCardHeader>
+                                <MDBCardHeader className='p-0 p-2 text-end fw-bold' >{data.split(': ')[0].slice(10)}</MDBCardHeader>
                                 {username.length >=  data.split(': ').slice(1).join(': ').length ?
                                 <MDBCardBody className='p-2 text-end'><div dangerouslySetInnerHTML={{ __html: data.split(': ').slice(1).join(': ') }}></div></MDBCardBody> :
                                 <MDBCardBody className='p-2'><div dangerouslySetInnerHTML={{ __html: data.split(': ').slice(1).join(': ') }}></div></MDBCardBody>}
@@ -209,7 +209,7 @@ const Home = ({ socket }) => {
                     <MDBRow start key={index} style={{marginRight: '20rem'}}>
                     <MDBCol md='auto' className='m-0'>
                             <MDBCard background='secondary' shadow='1' className=' my-1' style={{color: 'white'}}>
-                                <MDBCardHeader className='p-0 p-2' >{data.split(': ')[0]}</MDBCardHeader>
+                                <MDBCardHeader className='p-0 p-2 fw-bold' >{data.split(': ')[0]}</MDBCardHeader>
                                 <MDBCardBody className='p-2'><div dangerouslySetInnerHTML={{ __html: data.split(': ').slice(1).join(': ') }}></div></MDBCardBody>
                             </MDBCard>
                         </MDBCol>
