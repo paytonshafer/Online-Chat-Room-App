a50cc7053c8ef3d1bb77a687a1a026619c6a8c7c
paytonshafer <paytonshafer@gmail.com>
2023-10-09 20:34:17 -0400

    update todo

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 /*TODO:
 Upgrade UI to make interface look nice: login page, chat feed, message input, maybe logout screen
+    make messages send by user come up on one side vs other messages -> maybe remove colors then?
+        when username is changed all of the past messages get discolored
+    add tool tips - like on disabled buttons
+env files for front and backend to hold port, weather run is dev or deploy, 
 Set up some way to clear out logs
 select what "room" to join -> set a room string and then socket.join(room), io.in(room).emit()
 moderator/admin tools: mute user, delete messages -> do we need db for this? how to know a user is admin
@@ -45,7 +49,9 @@ const server = createServer(app);
 // create io instance of socket.io server and add cors route
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000"
+        //origin: "http://localhost:3000"
+        // add whitelist to allow more than 1 origin, add chloes ip
+        origin: "*" //uncomment and comment above to allow for any connection
     }
 })
 
