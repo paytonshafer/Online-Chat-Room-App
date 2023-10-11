6090d7f43be55f8b2f8f3de86844fb4bee3d4003
paytonshafer <paytonshafer@gmail.com>
2023-10-10 20:47:49 -0400

    add footer and plan

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import Footer from '../components/footer'
 import { useUserContext } from '../context/UserContext'; // get username context
+
+/*
+Welcome
+Jumbotron idrk
+room selection
+maybe carasol
+*/
+
 
 // login page
 const Home = ({ socket }) => {
     const { username } = useUserContext();
 
-    return(<h1>{username}'s home</h1>)
+    return(
+        <div>
+            <h1>{username}'s header</h1>
+            <h1>Info on app</h1>
+            <h1>Room selection</h1>
+            <h1>If extra content add carosol or something</h1>
+            <Footer />
+        </div>
+        
+    )
 }
 
 export default Home;
